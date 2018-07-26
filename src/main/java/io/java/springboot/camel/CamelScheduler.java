b7f305178f2ea92e3f64ba2eb0354becb0f12f7f
nsankabathula <naveen.c.s@hotmail.com>
2018-07-26 19:19:01 -0400

    mail changes

--- src/main/java/io/java/springboot/camel/CamelScheduler.java
+++ src/main/java/io/java/springboot/camel/CamelScheduler.java
@@ -52,7 +52,7 @@ public class CamelScheduler extends RouteBuilder{
                 .routeId(String.format( "%s.delimitedFileParser", name))
                 //.processRef("delimitedFileParser")
                 .to("log:DelimitedFileParser")
-                .to ("direct:CsvMarshal")
+                //.to ("direct:CsvMarshal")
                 .end();
 
         CsvDataFormat csv = new CsvDataFormat();
